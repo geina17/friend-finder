f99ff94d3ddf909fbf029544b02f49c0e849c841
Stefanie Loh <stefkloh@gmail.com>
2018-04-21 15:51:43 -0700

    Set up routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,5 +2,10 @@
 
 // Route that sends user to the survey page
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+
+// Route that sends user to the survey page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
   });
